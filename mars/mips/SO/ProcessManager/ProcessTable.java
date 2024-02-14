cfa86e24f5199c7b0d04e75df3e8e3d8b34db978
Breno Klywer Olegario de Moura <breno.moura@alunos.ufersa.edu.br>
2024-02-14 01:05:53 -0300

    TimerTool

--- mars/mips/SO/ProcessManager/ProcessTable.java
+++ mars/mips/SO/ProcessManager/ProcessTable.java
@@ -5,59 +5,70 @@ import java.util.LinkedList;
 import mars.mips.hardware.RegisterFile;
 
 public abstract class ProcessTable {
-    
-    // Initial generated PID value for new processes
-    private static int PID = 1;
-    
-    // Possible processes
-    private static Deque<ProcessControlBlock> readyProcesses = new LinkedList<>();
-    private static ProcessControlBlock executionProcess = new ProcessControlBlock(0, RegisterFile.getInitialProgramCounter(), ProcessControlBlock.ProcessState.RUNNING);
-    
-    public static Deque<ProcessControlBlock> getReadyProcesses() {
-        return readyProcesses;
-    }
-    
-    public static void setReadyProcesses(Deque<ProcessControlBlock> readyProcesses) {
-        ProcessTable.readyProcesses = readyProcesses;
-    }
-    
-    public static ProcessControlBlock getExecutionProcess() {
-        return executionProcess;
-    }
-    
-    public static void setExecutionProcess(ProcessControlBlock executionProcess) {
-        ProcessTable.executionProcess = executionProcess;
-    }
-    
-    public static int getPID() {
-        return PID++;
-    }
-    
-    /**
-     * Method for changing a process's state in the table.
-     *
-     * @param newState New process state
-     * @throws java.lang.Exception
-     */
-    public static void changeState(ProcessControlBlock.ProcessState newState) throws Exception {
-        executionProcess.setState(newState);
-    }
-    
-    /**
-     * Method for displaying all processes for debugging.
-     */
-    public static void listProcesses() {
-        System.out.println("PID\tAddress\tState");
 
-        for (ProcessControlBlock processo : readyProcesses) {
-            // Imprime na tela os dados do processo
-            System.out.println(processo.getPid() + "\t" + processo.getProgramAddress() + "\t" + processo.getState());
-        }
-        // Verifica se o processo em execução não é nulo
-        if (executionProcess != null) {
-            // Imprime na tela os dados do processo
-            System.out.println(executionProcess.getPid() + "\t" + executionProcess.getProgramAddress() + "\t" + executionProcess.getState());
-        }
+	// Initial generated PID value for new processes
+	private static int PID = 1;
+
+	// Possible processes
+	private static Deque<ProcessControlBlock> readyProcesses = new LinkedList<>();
+	private static ProcessControlBlock executionProcess = new ProcessControlBlock(0,RegisterFile.getInitialProgramCounter(), ProcessControlBlock.ProcessState.RUNNING);
+
+	public static Deque<ProcessControlBlock> getReadyProcesses() {
+		return readyProcesses;
+	}
+
+	public static void setReadyProcesses(Deque<ProcessControlBlock> readyProcesses) {
+		ProcessTable.readyProcesses = readyProcesses;
+	}
+
+	public static ProcessControlBlock getExecutionProcess() {
+		return executionProcess;
+	}
+
+	public static void setExecutionProcess(ProcessControlBlock executionProcess) {
+		ProcessTable.executionProcess = executionProcess;
+	}
+
+	public static int getPID() {
+		return PID++;
+	}
+
+	/**
+	 * Method for changing a process's state in the table.
+	 *
+	 * @param newState New process state
+	 * @throws java.lang.Exception
+	 */
+	public static void changeState(ProcessControlBlock.ProcessState newState) throws Exception {
+		executionProcess.setState(newState);
+	}
+
+	/**
+	 * Method for displaying all processes for debugging.
+	 */
+	public static void listProcesses() {
+		System.out.println("PID\tAddress\tState");
+
+		for (ProcessControlBlock processo : readyProcesses) {
+			// Imprime na tela os dados do processo
+			System.out.println(processo.getPid() + "\t" + processo.getProgramAddress() + "\t" + processo.getState());
+		}
+		// Verifica se o processo em execução não é nulo
+		if (executionProcess != null) {
+			// Imprime na tela os dados do processo
+			System.out.println(executionProcess.getPid() + "\t" + executionProcess.getProgramAddress() + "\t"
+					+ executionProcess.getState());
+		}
+	}
+
+	private static SchedulerE schedulingAlgorithm = SchedulerE.FIFO;
+
+    public static SchedulerE getSchedulingAlgorithm() {
+        return schedulingAlgorithm;
     }
-    
+
+    public static void setSchedulingAlgorithm(SchedulerE algorithm) {
+        schedulingAlgorithm = algorithm;
+    }
+
 }
--- mars/mips/SO/ProcessManager/Scheduler.java
+++ mars/mips/SO/ProcessManager/Scheduler.java
@@ -1,18 +1,91 @@
 package mars.mips.SO.ProcessManager;
 
+import java.util.Random;
+
 public abstract class Scheduler {
-    
-    /**
-     * Method for scheduling the next ready process.
-     */
-    public static void schedule() {
-        ProcessControlBlock nextProcess;
-        if (!ProcessTable.getReadyProcesses().isEmpty()) {
-            nextProcess = ProcessTable.getReadyProcesses().pop(); // Remove the process from ready process list
-            nextProcess.copyToHardware();// Retrieves the context to PCB
-            nextProcess.setState(ProcessControlBlock.ProcessState.RUNNING); // Change state
-            ProcessTable.setExecutionProcess(nextProcess); // Set the new process in execution
-        }
-    }
+
+	private static Random random = new Random();
+
+	/**
+	 * Method for scheduling the next ready process.
+	 */
+	public static void schedule() {
+		switch (ProcessTable.getSchedulingAlgorithm()) {
+		case FIFO:
+			scheduleFIFO();
+			break;
+		case PRIORITY:
+			schedulePriority();
+			break;
+		case LOTTERY:
+			scheduleLottery();
+			break;
+		default:
+			scheduleFIFO(); // Default to FIFO
+			break;
+		}
+	}
+
+	/**
+	 * Schedule process using First-In-First-Out algorithm.
+	 */
+	private static void scheduleFIFO() {
+		ProcessControlBlock nextProcess;
+		if (!ProcessTable.getReadyProcesses().isEmpty()) {
+			nextProcess = ProcessTable.getReadyProcesses().pop(); // Remove the process from ready process list
+			nextProcess.copyToHardware(); // Retrieves the context to PCB
+			nextProcess.setState(ProcessControlBlock.ProcessState.RUNNING); // Change state
+			ProcessTable.setExecutionProcess(nextProcess); // Set the new process in execution
+		}
+	}
+
+	/**
+	 * Schedule process using Fixed Priority algorithm.
+	 */
+	private static void schedulePriority() {
+		ProcessControlBlock highestPriorityProcess = null;
+		int highestPriority = Integer.MIN_VALUE;
+
+		for (ProcessControlBlock process : ProcessTable.getReadyProcesses()) {
+			if (process.getPriority() > highestPriority) {
+				highestPriority = process.getPriority();
+				highestPriorityProcess = process;
+			}
+		}
+
+		if (highestPriorityProcess != null) {
+			highestPriorityProcess.copyToHardware(); // Retrieves the context to PCB
+			highestPriorityProcess.setState(ProcessControlBlock.ProcessState.RUNNING); // Change state
+			ProcessTable.setExecutionProcess(highestPriorityProcess); // Set the new process in execution
+			ProcessTable.getReadyProcesses().remove(highestPriorityProcess); // Remove from ready queue
+		}
+	}
+
+	/**
+	 * Schedule process using Lottery algorithm.
+	 */
+	private static void scheduleLottery() {
+		if (!ProcessTable.getReadyProcesses().isEmpty()) {
+			int totalTickets = 0;
+			for (ProcessControlBlock process : ProcessTable.getReadyProcesses()) {
+				totalTickets += process.getPriority(); // Suponha que a prioridade do processo é usada como número de
+														// bilhetes
+			}
+
+			int winningTicket = random.nextInt(totalTickets);
+			int cumulativeTickets = 0;
+
+			for (ProcessControlBlock process : ProcessTable.getReadyProcesses()) {
+				cumulativeTickets += process.getPriority();
+				if (cumulativeTickets > winningTicket) {
+					process.copyToHardware(); // Retrieves the context to PCB
+					process.setState(ProcessControlBlock.ProcessState.RUNNING); // Change state
+					ProcessTable.setExecutionProcess(process); // Set the new process in execution
+					ProcessTable.getReadyProcesses().remove(process); // Remove from ready queue
+					break;
+				}
+			}
+		}
+	}
 
 }
