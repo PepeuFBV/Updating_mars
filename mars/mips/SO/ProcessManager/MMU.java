168f7c6d81def8b1fe525f7e8c3164d39b18e85d
BrenoKly <breno.moura@alunos.ufersa.edu.br>
2024-04-17 20:14:18 -0300

    MMU

--- mars/mips/SO/ProcessManager/MMU.java
+++ mars/mips/SO/ProcessManager/MMU.java
@@ -2,9 +2,7 @@ package mars.mips.SO.ProcessManager;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
-
 import mars.mips.hardware.AddressErrorException;
-import mars.simulator.Simulator;
 
 /**
  * Memory Management Unit
@@ -16,51 +14,25 @@ public abstract class MMU {
    */
   public static final Map<ProcessControlBlock, VirtualTableEntry[]> pageTable = new LinkedHashMap<>();
 
-  public boolean addPage(int[] addressInstructions) {
-    ProcessControlBlock process = ProcessTable.getExecutionProcess();
-
-    // Check if there is a running process
-    if (process == null) {
-      return false; // If not, cannot add the page
-    }
-
-    VirtualTableEntry[] tableEntries = pageTable.get(process);
-
-    // If the process doesn't have any associated blocks yet, create a new block
-    // array
-    if (tableEntries == null) {
-      tableEntries = new VirtualTableEntry[1];
-      tableEntries[0] = new VirtualTableEntry(addressInstructions);
-      pageTable.put(process, tableEntries);
-      return true;
-    }
-
-    // If the process already has associated blocks and there are already 16 blocks,
-    // return false
-    if (tableEntries.length >= 16) {
-      return false;
-    }
-
-    // If the process already has associated blocks and there are less than 16
-    // blocks, add the new block
-    VirtualTableEntry[] newBlocks = new VirtualTableEntry[tableEntries.length + 1];
-    System.arraycopy(tableEntries, 0, newBlocks, 0, tableEntries.length);
-    newBlocks[tableEntries.length] = new VirtualTableEntry(addressInstructions);
-    pageTable.put(process, newBlocks);
-    return true;
-  }
-
-  public static boolean addInstruction(int addressInstructions, int numPage, int displacement) {
+  public static void addInstruction(int addressInstructions, int numPage, int displacement) {
     ProcessControlBlock process = ProcessTable.getExecutionProcess();
     VirtualTableEntry[] pageTable = MMU.pageTable.get(process);
 
-    if (pageTable.length < MemoryManager.maxNumPages){
-      
+    if (pageTable.length < MemoryManager.maxNumPages) {
+      if (pageTable[numPage] == null) {
+        VirtualTableEntry newPage = new VirtualTableEntry();
+        newPage.addInstruction(addressInstructions, displacement);
+        newPage.setPresent(true);
+        pageTable[numPage] = newPage;
+        MMU.pageTable.put(process, pageTable);
+      } else {
+        pageTable[numPage].addInstruction(addressInstructions, displacement);
+        MMU.pageTable.put(process, pageTable);
+      }
     } else {
-      //  ALGORITMO DE SUBSTITUIÇÃO DE PÁGINA.
+      // ALGORITMO DE SUBSTITUIÇÃO DE PÁGINA.
+      process.setPageFaults(process.getPageFaults() + 1);
     }
-
-    return false;
   }
 
   public static void verifyInstruction(int address) throws AddressErrorException {
@@ -79,16 +51,7 @@ public abstract class MMU {
       addInstruction(address, index, displacement);
 
     } else {
-
-      if (address < process.getUpperLimit() || address > process.getLowerLimit()) {
-        String errorMessage = "\nThe address limits of the running process, "
-            + "which has an upper limit: " + process.getUpperLimit() + " \nand lower limit: "
-            + process.getLowerLimit() + " are outside the access area."
-            + "\nAccess attempt address: " + address + " ";
-        throw new AddressErrorException(errorMessage, Simulator.EXCEPTION, address);
-      }
-
-      if (pageTable[index].getInstructions()[displacement] == address) {
+      if (pageTable[index].getInstructions()[displacement] == address && pageTable[index].isPresent()) {
         process.setHits(process.getHits() + 1);
         pageTable[index].setReferencedPage(true);
         return;
@@ -98,12 +61,4 @@ public abstract class MMU {
       addInstruction(address, index, displacement);
     }
   }
-
-  // private static void pageFaultHandler(ProcessControlBlock process, int
-  // address, int pageNumber) {
-  // process.setPageFaults(process.getPageFaults() + 1);
-
-  // VirtualTableEntry[] pageTable = MMU.pageTable.get(process);
-  // int[] instructions = new int[MemoryManager.pageSize];
-  // }
 }
--- mars/mips/SO/ProcessManager/VirtualTableEntry.java
+++ mars/mips/SO/ProcessManager/VirtualTableEntry.java
@@ -3,7 +3,6 @@ package mars.mips.SO.ProcessManager;
 public class VirtualTableEntry {
 
   private int[] instructions;
-  private int cont;
 
   private boolean referencedPage;
   private boolean modifiedPage;
@@ -16,17 +15,12 @@ public class VirtualTableEntry {
   private boolean present;
   private int frameNumber;
 
-  public int getCont() {
-    return cont;
-  }
-
   public int[] getInstructions() {
     return instructions;
   }
 
   public VirtualTableEntry() {
     instructions = new int[MemoryManager.pageSize];
-    cont = 0;
   }
 
   public VirtualTableEntry(int[] instructions) {
@@ -35,98 +29,87 @@ public class VirtualTableEntry {
     }
     instructions = new int[MemoryManager.pageSize];
     System.arraycopy(instructions, 0, this.instructions, 0, instructions.length);
-    cont = instructions.length;
   }
 
-  public void addInstruction(int address) {
-    if (cont < MemoryManager.pageSize) {
-      instructions[cont] = address;
-      cont++;
-    }
+  public void addInstruction(int address, int displacement) {
+    instructions[displacement] = address;
   }
 
-    /**
-     * @param instructions the instructions to set
-     */
-    public void setInstructions(int[] instructions) {
-        this.instructions = instructions;
-    }
+  /**
+   * @param instructions the instructions to set
+   */
+  public void setInstructions(int[] instructions) {
+    this.instructions = instructions;
+  }
 
-    /**
-     * @param cont the cont to set
-     */
-    public void setCont(int cont) {
-        this.cont = cont;
-    }
+  /**
+   * @return boolean return the referencedPage
+   */
+  public boolean isReferencedPage() {
+    return referencedPage;
+  }
 
-    /**
-     * @return boolean return the referencedPage
-     */
-    public boolean isReferencedPage() {
-        return referencedPage;
-    }
+  /**
+   * @param referencedPage the referencedPage to set
+   */
+  public void setReferencedPage(boolean referencedPage) {
+    this.referencedPage = referencedPage;
+  }
 
-    /**
-     * @param referencedPage the referencedPage to set
-     */
-    public void setReferencedPage(boolean referencedPage) {
-        this.referencedPage = referencedPage;
-    }
+  /**
+   * @return boolean return the modifiedPage
+   */
+  public boolean isModifiedPage() {
+    return modifiedPage;
+  }
 
-    /**
-     * @return boolean return the modifiedPage
-     */
-    public boolean isModifiedPage() {
-        return modifiedPage;
-    }
+  /**
+   * @param modifiedPage the modifiedPage to set
+   */
+  public void setModifiedPage(boolean modifiedPage) {
+    this.modifiedPage = modifiedPage;
+  }
 
-    /**
-     * @param modifiedPage the modifiedPage to set
-     */
-    public void setModifiedPage(boolean modifiedPage) {
-        this.modifiedPage = modifiedPage;
-    }
+  /**
+   * @return protectionBits return the protection
+   */
+  public protectionBits getProtection() {
+    return protection;
+  }
 
-    /**
-     * @return protectionBits return the protection
-     */
-    public protectionBits getProtection() {
-        return protection;
-    }
+  /**
+   * @param protection the protection to set
+   */
+  public void setProtection(protectionBits protection) {
+    this.protection = protection;
+  }
 
-    /**
-     * @param protection the protection to set
-     */
-    public void setProtection(protectionBits protection) {
-        this.protection = protection;
-    }
+  /**
+   * @return boolean return the present
+   */
+  public boolean isPresent() {
+    return present;
+  }
 
-    /**
-     * @return boolean return the present
-     */
-    public boolean isPresent() {
-        return present;
-    }
+  /**
+   * @param present the present to set
+   */
+  public void setPresent(boolean present) {
+    this.present = present;
+  }
 
-    /**
-     * @param present the present to set
-     */
-    public void setPresent(boolean present) {
-        this.present = present;
-    }
+  /**
+   * @return int return the frameNumber
+   */
+  public int getFrameNumber() {
+    return frameNumber;
+  }
 
-    /**
-     * @return int return the frameNumber
-     */
-    public int getFrameNumber() {
-        return frameNumber;
-    }
-
-    /**
-     * @param frameNumber the frameNumber to set
-     */
-    public void setFrameNumber(int frameNumber) {
-        this.frameNumber = frameNumber;
-    }
+  /**
+   * @param frameNumber the frameNumber to set
+   */
+  public void setFrameNumber(int frameNumber) {
+    this.frameNumber = frameNumber;
+  }
 
 }
