5094e73817da6adb99a493e52850d144c3de06ce
BrenoKly <breno.moura@alunos.ufersa.edu.br>
2024-04-18 02:23:44 -0300

    update

--- mars/mips/SO/ProcessManager/Scheduler.java
+++ mars/mips/SO/ProcessManager/Scheduler.java
@@ -4,45 +4,31 @@ import java.util.Random;
 
 public abstract class Scheduler {
 
-	private static Random random = new Random();
+	private static final Random random = new Random();
 
-	/**
-	 * Method for scheduling the next ready process.
-	 */
 	public static void schedule() {
 		switch (ProcessTable.getSchedulingAlgorithm()) {
-		case FIFO:
-			scheduleFIFO();
-			break;
-		case PRIORITY:
-			schedulePriority();
-			break;
-		case LOTTERY:
-			scheduleLottery();
-			break;
-		default:
-			scheduleFIFO(); // Default to FIFO
-			break;
+			case FIFO -> fifo();
+			case PRIORITY -> priority();
+			case LOTTERY -> lottery();
 		}
 	}
 
 	/**
 	 * Schedule process using First-In-First-Out algorithm.
 	 */
-	private static void scheduleFIFO() {
-		ProcessControlBlock nextProcess;
-		if (!ProcessTable.getReadyProcesses().isEmpty()) {
-			nextProcess = ProcessTable.getReadyProcesses().pop(); // Remove the process from ready process list
-			nextProcess.copyToHardware(); // Retrieves the context to PCB
-			nextProcess.setState(ProcessControlBlock.ProcessState.RUNNING); // Change state
-			ProcessTable.setExecutionProcess(nextProcess); // Set the new process in execution
-		}
+	public static void fifo() {
+		// Remove the process from ready process list
+		ProcessControlBlock nextProcess = ProcessTable.getReadyProcesses().pop();
+		nextProcess.copyToHardware(); // Retrieves the context to PCB
+		nextProcess.setState(ProcessControlBlock.ProcessState.RUNNING); // Change state
+		ProcessTable.setExecutionProcess(nextProcess); // Set the new process in execution
 	}
 
 	/**
 	 * Schedule process using Fixed Priority algorithm.
 	 */
-	private static void schedulePriority() {
+	public static void priority() {
 		ProcessControlBlock highestPriorityProcess = null;
 		int highestPriority = Integer.MIN_VALUE;
 
@@ -54,36 +40,33 @@ public abstract class Scheduler {
 		}
 
 		if (highestPriorityProcess != null) {
-			highestPriorityProcess.copyToHardware(); // Retrieves the context to PCB
-			highestPriorityProcess.setState(ProcessControlBlock.ProcessState.RUNNING); // Change state
-			ProcessTable.setExecutionProcess(highestPriorityProcess); // Set the new process in execution
 			ProcessTable.getReadyProcesses().remove(highestPriorityProcess); // Remove from ready queue
+			highestPriorityProcess.copyToHardware(); // Retrieves the context to PCB
+			ProcessTable.setExecutionProcess(highestPriorityProcess); // Set the new process in execution
+			highestPriorityProcess.setState(ProcessControlBlock.ProcessState.RUNNING); // Change state
 		}
 	}
 
 	/**
 	 * Schedule process using Lottery algorithm.
 	 */
-	private static void scheduleLottery() {
-		if (!ProcessTable.getReadyProcesses().isEmpty()) {
-			int totalTickets = 0;
-			for (ProcessControlBlock process : ProcessTable.getReadyProcesses()) {
-				totalTickets += process.getPriority(); // Suponha que a prioridade do processo é usada como número de
-														// bilhetes
-			}
+	public static void lottery() {
+		int totalTickets = 0;
+		for (ProcessControlBlock process : ProcessTable.getReadyProcesses()) {
+			totalTickets += process.getPriority();
+		}
 
-			int winningTicket = random.nextInt(totalTickets);
-			int cumulativeTickets = 0;
+		int winningTicket = random.nextInt(totalTickets);
+		int cumulativeTickets = 0;
 
-			for (ProcessControlBlock process : ProcessTable.getReadyProcesses()) {
-				cumulativeTickets += process.getPriority();
-				if (cumulativeTickets > winningTicket) {
-					process.copyToHardware(); // Retrieves the context to PCB
-					process.setState(ProcessControlBlock.ProcessState.RUNNING); // Change state
-					ProcessTable.setExecutionProcess(process); // Set the new process in execution
-					ProcessTable.getReadyProcesses().remove(process); // Remove from ready queue
-					break;
-				}
+		for (ProcessControlBlock process : ProcessTable.getReadyProcesses()) {
+			cumulativeTickets += process.getPriority();
+			if (cumulativeTickets > winningTicket) {
+				process.copyToHardware(); // Retrieves the context to PCB
+				process.setState(ProcessControlBlock.ProcessState.RUNNING); // Change state
+				ProcessTable.setExecutionProcess(process); // Set the new process in execution
+				ProcessTable.getReadyProcesses().remove(process); // Remove from ready queue
+				break;
 			}
 		}
 	}
